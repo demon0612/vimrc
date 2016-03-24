0b476c8bfe22ae65e00e50bc90fa424ee49cf400
demon0612 <zanghongliang@qq.com>
2016-03-24 22:26:46 +0800

    add install script

---
+++ install.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+command -v ctags >/dev/null 2>&1 || { echo >&2 "ctags is missing.Please install first"; exit 1; }  
+command -v python >/dev/null 2>&1 || { echo >&2 "python is missing.Please install first"; exit 1; }  
+command -v ruby >/dev/null 2>&1 || { echo >&2 "ruby is missing.Please install first"; exit 1; }  
+command -v clang >/dev/null 2>&1 || { echo >&2 "clang is missing.Please install first"; exit 1; }  
+
+rm -rf ~/.vim
+rm -rf ~/.vimrc
+ln -sf $PWD ~/.vim;
+#ln -sf $PWD/vimrcs/vimrc ~/.vimrc;
+cp ./vimrcs/vimrc ~/.vimrc
+if [ ! -d temps ];
+then
+    mkdir temps
+fi
+
+
+#checkVi=`vim --version|grep +python`
+#if [ "x$checkVi" == "x" ]
+#then
+    #echo "Vim is not complied with python, please rebuild it before install"
+    #exit
+#fi
+
+#git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+#cd ~/.vim/
+#https://github.com/powerline/fonts
+#cd fonts
+#./install.sh
+
+#vim +PluginInstall +qall
+
+#cd ~/.vim/bundle/YouCompleteMe
+#./install.sh --clang-completer --system-libclang
+
+#mkdir -p ~/.vim/temps
+
